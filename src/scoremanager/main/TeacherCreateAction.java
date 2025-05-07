41e2943b2c1fc36167360e10718c66a468e3208b
yota0814 <tky2402136@stu.o-hara.ac.jp>
2025-05-07 12:05:50 +0900

    先生リストjavaと登録

---
+++ src/scoremanager/main/TeacherCreateAction.java
@@ -0,0 +1,19 @@
+// 新規登録のためjspへ
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class TeacherCreateAction extends Action {
+
+    @Override
+    public void execute(
+        HttpServletRequest req, HttpServletResponse res
+    ) throws Exception {
+
+        req.getRequestDispatcher("teacher_create.jsp").forward(req, res);
+    }
+
+}
