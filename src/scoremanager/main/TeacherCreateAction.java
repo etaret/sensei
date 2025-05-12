49f2898961f86f351e2ae8db9b1afd2bf85c8eda
tsuten <hiimhereforyouguys@gmail.com>
2025-05-12 10:42:37 +0900

    インポートし忘れ修正

--- src/scoremanager/main/TeacherCreateAction.java
+++ src/scoremanager/main/TeacherCreateAction.java
@@ -3,6 +3,8 @@ package scoremanager.main;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import bean.Teacher;
 
 import tool.Action;
 
